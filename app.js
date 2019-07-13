db222830b88a89525e6bcd32bf02368eee333a5f
abenezermonjor <monjo003@morris.umn.edu>
2019-07-13 15:49:22 -0500

    making adjestment for the page to have a single footer and header and also connect the pages on the server

--- app.js
+++ app.js
@@ -12,10 +12,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-app.get("/", function(req, res) {
 
-res.render("index"); 
+app.get("/", function(req, res) {
+res.render("index");
 });
+
+
+
+app.get("/contact", function(req, res) {
+res.render(__dirname+'/views/contact');
+});
+
+
+app.get("/categories", function(req, res) {
+res.render(__dirname+'/views/categories');
+});
+
+
+
+app.get("/single", function(req, res) {
+res.render(__dirname+'/views/single');
+});
+
+
+
 
 app.listen(4000, function() {
   console.log("Server stated on port 4000");
